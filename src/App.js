d3d64b2b6c24b86eae9fc3d7481e951e8747a2ab
“Fareedh <2617283y@student.gla.ac.uk>
2022-08-09 15:56:26 +0100

    fixed flags and used api

--- src/App.js
+++ src/App.js
@@ -3,36 +3,49 @@ import CountryCard from './components/CountryCard';
 import Heading from './components/Heading';
 import RegionFilter from './components/RegionFilter';
 import SearchInput from './components/SearchInput';
-import allCountries from './countriesAll.json'
-import { useState } from 'react';
+import { useState , useEffect } from 'react';
 
 function App() {
-    const [countries, setCountries] = useState(allCountries);
+    const [countries, setCountries] = useState(null);
+    const [countriesToSearch, setCountriesToSearch] = useState(null)
+    const [region, setRegion] = useState(null);
 
-    //search functionality
+    useEffect (()=>{
+      fetch(`https://restcountries.com/v3.1/${region ? 'region/' + region : 'all' }`)
+      .then((res)=> res.json())
+      .then((data) => {
+        setCountries(data)
+        setCountriesToSearch(data)
+      })
+    }, [region])
+    // search functionality
     function onSearch(countryName){
-      setCountries(allCountries.filter((country)=>{return country.name.toLowerCase().includes(countryName.toLowerCase())}));
+      setCountries(countriesToSearch.filter((country)=>{return country.name.common.toLowerCase().includes(countryName.toLowerCase())}));
     }
 
     //filter functionality
     function onFilter(filteredRegion){
-      setCountries(filteredRegion !== 'All' ? allCountries.filter((country) => country.region === filteredRegion): allCountries);
+      setRegion(filteredRegion !== 'All' && filteredRegion);
     }
+
+    //Display country functionality
+
+
   return (
     <div className="App">
+
       <Heading/>
       <div className='container contents'>
         <div className = "d-flex d-flex justify-content-between mb-5" >
-        <SearchInput onSearch = {onSearch}/>
-        <RegionFilter onFilter = {onFilter}/>
-      </div>
+          <SearchInput onSearch = {onSearch}/>
+          <RegionFilter onFilter = {onFilter}/>
+        </div>
       
-      <div className="d-flex flex-wrap justify-content-between">
-        {countries.map((country)=>(
-        <CountryCard country = {country} key = {country.name}/>
-      ))}
-      
-      </div>
+        <div className="d-flex flex-wrap justify-content-between">
+          {countries && countries.map((country)=>(
+          <CountryCard country = {country} key = {country.name.common}/>
+        ))}
+        </div>
       </div>
     </div>
   );
--- src/components/CountryCard.js
+++ src/components/CountryCard.js
@@ -3,9 +3,9 @@ import React from 'react'
 const CountryCard = (props) => {
   return (
         <div className="card h-100 mb-5" style={{width: 25+'rem',}}>
-        <img src={props.country.flag} className="card-img-top" alt={props.country.name + " flag."}/>
+        <img src={props.country.flags.png} className="card-img-top" alt={props.country.name.common + " flag."}/>
         <div className="card-body">
-            <h5 className="card-title">{props.country.name}</h5>
+            <h5 className="card-title">{props.country.name.common}</h5>
         </div>
         <div className="card-body cardBody">
             <p className="card-text">Population: <small className="text-muted">{props.country.population}</small></p>
